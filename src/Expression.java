fb0241befd0f7b4b3ad366fb859d68c53992978a
tt24 <tt24@st-andrews.ac.uk>
2016-04-08 17:00:02 +0300

    home

--- src/Expression.java
+++ src/Expression.java
@@ -1,24 +1,48 @@
 
 public class Expression {
-	private DecisionVariable var;
-	private Operator operator;
-	private int value;
-	
-	public Expression(DecisionVariable var, Operator operator, int value) {
-		this.var = var;
-		this.operator = operator;
-		this.value = value;
-	}
-	
-	public DecisionVariable getVar() {
-		return this.var;
-	}
-	public Operator getOperator() {
-		return this.operator;
-	}
-	public int getValue() {
-		return this.value;
-	}
-	
+
+    private DecisionVariable var;
+    private Operator operator;
+    private int value;
+    private boolean order;
+
+    public Expression(DecisionVariable var, Operator operator, int value, boolean order) {
+        this.var = var;
+        this.operator = operator;
+        this.value = value;
+        this.order = order;
+    }
+
+    public Expression(DecisionVariable var, boolean order) {
+        this.var = var;
+        this.order = order;
+    }
+
+    public DecisionVariable getVar() {
+        return this.var;
+    }
+
+    public Operator getOperator() {
+        return this.operator;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+    
+    public boolean getOrder() {
+        return this.order;
+    }
+
+    public int solve(int varValue) {
+        return 0;
+    }
+    
+    public String toString() {
+        if(this.operator!=null) {
+            return this.var.getName()+" "+operator+" "+this.value+" "+this.order;
+        }
+        return this.var.getName();
+    }
 
 }
--- src/ProblemReader.java
+++ src/ProblemReader.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.TreeSet;
 
-import javax.naming.RefAddr;
 
 public class ProblemReader {
 	
@@ -47,12 +50,130 @@ public class ProblemReader {
 		}
 		return new Matrix(name, matrix);		
 	}
+        
+        public static Constraint readContraint(String s, ArrayList<DecisionVariable> variables) {
+            s = s.replace(" ", "");
+            Comparator comparator;
+            String parts[];
+            if(s.contains("<=")) {
+                comparator=Comparator.LEQ;
+                parts=s.split("<=");
+            }
+            else {
+                if(s.contains("<")) {
+                    comparator=Comparator.LESS;
+                    parts=s.split("<");
+                }
+                else {
+                    if(s.contains(">=")) {
+                        comparator=Comparator.MEQ;
+                        parts=s.split(">=");
+                    }
+                    else {
+                        if(s.contains(">")) {
+                            comparator=Comparator.MORE;
+                            parts=s.split(">");
+                        }
+                        else {
+                            if(s.contains("==")) {
+                                comparator=Comparator.EQ;
+                                parts=s.split("==");
+                            }
+                            else {
+                                comparator=Comparator.NEQ;
+                                parts=s.split("!=");
+                            }
+                        }
+                    }
+                }
+            }
+            return new Constraint(readExpression(parts[0], variables), readExpression(parts[1], variables), comparator);
+        }
+        
+        @SuppressWarnings("empty-statement")
+        public static Expression readExpression(String s, ArrayList<DecisionVariable> variables) {
+            Operator operator = null;
+            String[] parts = {s};
+            if(s.contains("+")) {
+                operator = Operator.PLUS;
+                parts = s.split("+");
+            }
+            else {
+                if(s.contains("-")) {
+                    operator = Operator.MINUS;
+                    parts = s.split("-");
+                }
+                else {
+                    if(s.contains("*")) {
+                        operator = Operator.MULTIPLY;
+                        parts = s.split("*");
+                    }
+                    else {
+                        if(s.contains("/")) {
+                            operator = Operator.DIVIDE;
+                            parts = s.split("/");
+                        }
+                    }
+                }
+            }
+            Expression expression = null;
+            for(DecisionVariable var: variables) {
+                if(parts[0].equals(var.getName())) {
+                    if(parts.length>1) {
+                        expression = new Expression(var, operator, Integer.parseInt(parts[1]), true);
+                    }
+                    else {
+                        expression = new Expression(var, true);
+                    }
+                }
+                else {
+                    if(parts.length>1 && parts[1].equals(var.getName())) {
+                        expression = new Expression(var, operator, Integer.parseInt(parts[0]), false);
+                    }
+                    else {
+                        expression = new Expression(var, false);
+                    }
+                }
+            }
+            return expression;
+        }
+        
+        public static DecisionVariable readVariable(String s) {
+            
+            return null;
+        }
+        
+        public static void readProblem(String filename) throws FileNotFoundException, IOException {
+            String let = "let";
+            String find = "find";
+            BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+            String s;
+            ArrayList<Constraint> constraints = new ArrayList<>();
+            ArrayList<DecisionVariable> variables = new ArrayList<>();
+            while((s=br.readLine())!=null) {
+                if(s.contains(let)) {
+                    s = s.replace(let, "");
+                    variables.add(readVariable(s));
+                }
+                else {
+                    if(s.contains(find)) {
+                        
+                    }
+                    else {
+                        constraints.add(readContraint(s,variables));
+                        System.out.println(constraints.get(constraints.size()-1));
+                    }
+                }
+            }
+        }
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String dom = "int(1-10)";
 		String list = "[1 8 6]";
 		TreeSet<Integer> domain = readRange(dom);
 		System.out.println(createMatrix("mm", 3,2,domain));
+                String filename = "check.txt";
+                readProblem(filename);
 	}
 
 }
